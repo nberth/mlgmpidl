454e47fc790156fa8bb884594c57a5c4a7fcb93a
antoinemine <antoinemine@c27d38ec-fa33-0410-b621-fb01b97e5d69>
2015-09-21 09:02:08 +0000

    - configure script to automtically generate Makefile.config - GNU sed macros replaced with perl
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/mlxxxidl/mlgmpidl/trunk@328 c27d38ec-fa33-0410-b621-fb01b97e5d69

---
+++ perlscript_c.pl
@@ -0,0 +1,7 @@
+print "\n/* This file is part of the MLGmpIDL interface, released under LGPL license.\n  Please read the COPYING file packaged in the distribution  */";
+while (<>) {
+    s/([^ ]*)_ptr OUTOUTOUT;/$1_t OUTOUTOUT; $1_ptr OUTTOUTTOUTT = OUTOUTOUT;/g;
+    s/\(\&OUTOUTOUT/\(\&OUTTOUTTOUTT/g;  
+    s/struct mpfr_rnd_t/mpfr_rnd_t/g;
+    print;
+}
